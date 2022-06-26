2fa0465eec926ef6bf3f9861ec85d6c86f3c84cc
Xcal1burBG <99122623+Xcal1burBG@users.noreply.github.com>
2022-06-26 17:46:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> shopQueue = new Queue<String>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input != "Paid") //input = next person in the queue
+                {
+                    shopQueue.Enqueue(input);
+                }
+
+                else //input = "Paid"
+
+                    for (int i = 0; i < shopQueue.Count; i++)
+                    {
+                        Console.WriteLine(shopQueue.Peek());
+                        shopQueue.Dequeue();
+                        i--;
+                    }
+
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"{shopQueue.Count} people remaining.");
+
+        }
+
+    }
+}
+
